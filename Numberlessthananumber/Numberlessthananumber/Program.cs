d8cb58bedf97899106accfa165179ba5af26c68b
Pradeep Sambu <36373241+pradeep0024@users.noreply.github.com>
2020-03-13 15:01:58 -0400

    Add files via upload

---
+++ Numberlessthananumber/Numberlessthananumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Conditional
+{
+	class IfStatement
+	{
+		public static void Main(string[] args)
+		{
+			int number = 2;
+			if (number < 5)
+			{
+				Console.WriteLine("{0} is less than 5", number);
+			}
+
+			Console.WriteLine("This statement is always executed.");
+		}
+	}
---
+++ findaverage/findaverage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace findaverage
+{
+    class Demo
+    {
+        static void Main()
+        {
+            var arr = new int[] { 10, 17, 25, 30, 40, 55, 60, 70 };
+            double avg = Queryable.Average(arr.AsQueryable());
+            Console.WriteLine("Average = " + avg);
+        }
+    }
+}
