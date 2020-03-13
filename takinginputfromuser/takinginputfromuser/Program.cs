1c8f39e9c6b22ed1eb8797716fa8f33e2d1e5730
Pradeep Sambu <36373241+pradeep0024@users.noreply.github.com>
2020-03-13 15:04:33 -0400

    Add files via upload

---
+++ takinginputfromuser/takinginputfromuser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace takinginputfromuser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Age");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("you are " + number + "years old");
+        }
+    }
+}
---
+++ whileloop/whileloop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the target");
+            int target = int.Parse(Console.ReadLine());
+
+            int start = 0;
+
+            while (start <= target)
+            {
+                Console.Write
+                    (start + " ");
+                start = start + 2;
+            }
+
+
+        }
+    }
+}
