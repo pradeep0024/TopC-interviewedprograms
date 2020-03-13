8304c4762aeae3b762f73ea6202a243fffbedef1
Pradeep Sambu <36373241+pradeep0024@users.noreply.github.com>
2020-03-13 14:59:20 -0400

    Add files via upload

---
+++ ConvertingtoASCIIcharacter/ConvertingtoASCIIcharacter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CharASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char character;
+            Console.Write("Enter the character: ");
+            character = Char.Parse(Console.ReadLine());
+            Console.WriteLine("The ASCII value of '" + character + "' using (int)character: " + (int)character);
+            byte[] charByte = Encoding.ASCII.GetBytes(character.ToString());
+            Console.WriteLine("The ASCII value of '" + character + "' using Encoding.ASCII.GetBytes(): " + charByte[0]);
+            Console.ReadLine();
+        }
+    }
+}
